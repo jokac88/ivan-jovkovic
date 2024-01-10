cf20d7cefc08e00f3087843fa5b35b966398aff0
jokac88 <ivan.jovkovic88@gmail.com>
2024-01-10 19:25:22 +0100

    Updates

--- _nuxt/index.1M09NclG.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./App.8035biCr.js";import{u as e,g as n,o as r}from"./entry.LXBy2oxZ.js";import"./Icon.uUyjajdv.js";import"./nuxt-link.qYQzKqa9.js";const l={__name:"index",setup(_){return e({htmlAttrs:{lang:"en"},title:"Ivan Jovkovic | Front-End Developer"}),(p,a)=>{const o=t;return r(),n(o)}}};export{l as default};
