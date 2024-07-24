8a742d5ce25baf2871cb935dc68ae0969a94da0e
jokac88 <ivan.jovkovic88@gmail.com>
2024-07-24 20:21:44 +0200

    Updates

---
+++ _nuxt/BQRMEhW8.js
@@ -0,0 +1,1 @@
+import{f as e,u as o,h as t,o as r}from"./BLHBb8DN.js";import{_ as n}from"./tu5fxfCy.js";import"./CkgVmzGK.js";import"./BKKLtQiM.js";import"./BzgEsGdh.js";const l=e({__name:"index",setup(a){return o({htmlAttrs:{lang:"en"},title:"Ivan Jovkovic | Front-End Developer"}),(s,m)=>(r(),t(n))}});export{l as default};
